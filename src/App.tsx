26fd45583a72cb758f8ffadc4f15554c55b580d8
Rishika Dhanawade <rishika.dhanawade@gmail.com>
2025-09-18 18:15:59 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { CompliancePortalDashboard } from "./components/CompliancePortalDashboard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <CompliancePortalDashboard />
+    </div>
+  );
+}
